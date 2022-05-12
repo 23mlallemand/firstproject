f7033bb7b0bd14e673624375b48bf7ce69a2c6a5
23mlallemand <105305296+23mlallemand@users.noreply.github.com>
2022-05-12 08:59:09 -0700

    Update droneflight.py

--- droneflight.py
+++ droneflight.py
@@ -2,16 +2,18 @@ from djitellopy import tello
 from time import sleep
 
 drone = tello.Tello()
-# Drone will take off and move up to proper altitude
+drone.connect()
+
+# Drone will take off and move up to proper altitude (170cm)
 drone.takeoff()
 drone.move_up(90)
 # Drone will move forward 800cm in two calls then sleep
 drone.move_forward(400)
 drone.move_forward(400)
 sleep(.5)
-# Drone will turn to the left and continue forward to the landing zone in 500cm
+# Drone will turn to the left 90 degrees and continue forward to the landing zone in 500cm
 drone.rotate_counter_clockwise(90)
 drone.move_forward(500)
 sleep(.5)
-# Drone will land
+# Drone will land in the landing zone
 drone.land()
